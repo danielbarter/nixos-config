9b78b092f4c7bb8c749e8be029efbad491b9dad9
Daniel Barter <danielbarter@gmail.com>
2022-04-12 13:50:14 -0700

    installing android-studio from local nixpkgs

--- danielbarterEnv.nix
+++ danielbarterEnv.nix
@@ -1,21 +1,23 @@
-# I use user environments to install packages with overlays, since it is kind of
+# I use user environments to install packages from overlays and local nixpkgs
 # messy to do it in configuration.nix
 # refresh with nix-env --remove-all -if /etc/nixos/danielbarterEnv.nix
 
-with import <nixpkgs> {
+let
+  pkgsEmacsOverlay =  import <nixpkgs> {
+    # emacs overlay to get pureGTK emacs
+    # current mainline emacs isn't wayland native
+    # this is a problem if you are using multiple screens with different resolutions
+    # this community overlay adds developer versions of emacs with wayland support
+    # amoung other things.
+    overlays = [
+      (import (builtins.fetchTarball {
+        url = https://github.com/nix-community/emacs-overlay/archive/master.tar.gz;
+      }))
+    ];
+  };
 
-  # emacs overlay to get pureGTK emacs
-  # current mainline emacs isn't wayland native
-  # this is a problem if you are using multiple screens with different resolutions
-  # this community overlay adds developer versions of emacs with wayland support
-  # amoung other things.
-  overlays = [
-    (import (builtins.fetchTarball {
-      url = https://github.com/nix-community/emacs-overlay/archive/master.tar.gz;
-    }))
-  ];
-
-};
-[
-    emacsPgtk
-]
+  pkgsLocal = import /home/danielbarter/nixpkgs {};
+  in [
+    pkgsEmacsOverlay.emacsPgtk
+    (pkgsLocal.android-studio.override { tiling_wm = true;})
+  ]
