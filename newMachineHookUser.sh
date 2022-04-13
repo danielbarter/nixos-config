cc3feca1f6fdff5fd09e5bdd472422a48c0146c9
Daniel Barter <danielbarter@gmail.com>
2022-04-13 09:37:17 -0700

    intellij won't follow symlinks outside of home

--- newMachineHookUser.sh
+++ newMachineHookUser.sh
@@ -12,7 +12,11 @@ mkdir /home/danielbarter/.ssh/sockets
 
 
 ln -s /etc/nixos/dotfiles/emacs/emacs.el /home/danielbarter/.emacs.el
-ln -s /etc/nixos/dotfiles/vim/vimrc /home/danielbarter/.vimrc
+
+# IntelliJ won't read symbolic links which point outside its home directory
+ln /etc/nixos/dotfiles/vim/vimrc /home/danielbarter/.vimrc
+ln /etc/nixos/dotfiles/vim/ideavimrc /home/danielbarter/.ideavimrc
+
 ln -s /etc/nixos/dotfiles/alacritty/alacritty.yml /home/danielbarter/.alacritty.yml
 ln -s /etc/nixos/dotfiles/alacritty/bashrc /home/danielbarter/.bashrc
 
