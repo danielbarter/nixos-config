4690b60226285996d6a536b42b022697c5d80e41
Daniel Barter <danielbarter@gmail.com>
2022-05-20 07:57:36 -0700

    using networkd for dhcp

--- configuration.nix
+++ configuration.nix
@@ -103,6 +103,8 @@ in
       enable = true;
     };
 
+    useNetworkd = true;
+
     # The global useDHCP flag is deprecated, therefore explicitly set to false here.
     # Per-interface useDHCP will be mandatory in the future, so this generated config
     # replicates the default behaviour.
