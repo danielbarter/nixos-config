c299ffa8220387ae8fdc7c916362bd433858d05f
Daniel Barter <danielbarter@gmail.com>
2022-08-27 16:31:00 -0700

    powersaving is too agressive with usb suspend

--- host-specific/jasper/configuration.nix
+++ host-specific/jasper/configuration.nix
@@ -3,26 +3,7 @@
 {
   services.logind.lidSwitch = "suspend";
 
-  # service for improving battery life
-  services.tlp = {
-    enable = true;
-    settings = {
-      PCIE_ASPM_ON_BAT = "powersupersave";
-    };
-  };
-
-  systemd.user.services.powertop = {
-    description = "pueued daemon";
-    bindsTo = [ "default.target" ];
-    wants = [ "default.target" ];
-    after = [ "default.target" ];
-    wantedBy = [ "multi-user.target" ];
-    serviceConfig = {
-      Type = "oneshot";
-      ExecStart = '' ${pkgs.powertop}/bin/powertop --auto-tune'';
-    };
-  };
-
+  # utility for controlling power settings
   powerManagement.powertop.enable = true;
 
   # make udev rules for backlight
