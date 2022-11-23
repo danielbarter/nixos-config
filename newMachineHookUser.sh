522677f12df9d04b114d71dd0bda5e07779a5f54
Daniel Barter <danielbarter@gmail.com>
2022-11-22 20:36:29 -0800

    emacs should be at top of newMachineHookUser.sh

--- newMachineHookUser.sh
+++ newMachineHookUser.sh
@@ -1,6 +1,8 @@
 # setting up home directory
 # run as danielbarter
 
+ln -s /etc/nixos/dotfiles/emacs/emacs.el /home/danielbarter/.emacs.el
+
 gpg --import /etc/nixos/secrets/gpg/privkey.asc /etc/nixos/secrets/gpg/public.asc
 
 # setup ssh
@@ -9,9 +11,6 @@ ln -s /etc/nixos/secrets/ssh/id_rsa /home/danielbarter/.ssh/
 ln -s /etc/nixos/secrets/ssh/id_rsa.pub /home/danielbarter/.ssh
 ln -s /etc/nixos/dotfiles/ssh/config /home/danielbarter/.ssh
 mkdir /home/danielbarter/.ssh/sockets
-
-
-ln -s /etc/nixos/dotfiles/emacs/emacs.el /home/danielbarter/.emacs.el
 
 # IntelliJ won't read symbolic links which point outside its home directory
 ln /etc/nixos/dotfiles/vim/vimrc /home/danielbarter/.vimrc
