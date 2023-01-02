5acef72c34fa949d55bea8f4cfc165ca5fc23769
Daniel Barter <danielbarter@gmail.com>
2023-01-01 19:24:13 -0800

    more precise ownership for secrets

--- newMachineHookRoot.sh
+++ newMachineHookRoot.sh
@@ -3,7 +3,21 @@
 # run before newMachineHookUser.sh as it sets a bunch of permissions
 
 
+
 chown -R danielbarter /etc/nixos
-chgrp -R users /etc/nixos
-chmod 600 /etc/nixos/secrets/gpg/*
-chmod 600 /etc/nixos/secrets/ssh/*
+chgrp -R wheel /etc/nixos
+
+# LAN cache private key needs to be owned by nix-serve
+chown nix-serve /etc/nixos/secrets/binary-cache/cache-priv-key.pem
+chgrp nix-serve /etc/nixos/secrets/binary-cache/cache-priv-key.pem
+
+for file in $(find /etc/nixos/secrets -type f)
+do
+    chmod 600 $file
+done
+
+
+for file in $(find /etc/nixos/secrets -type d)
+do
+    chmod 700 $file
+done
