84cae7c211dbae091f51d3a2ba983bc251a44b77
Daniel Barter <danielbarter@gmail.com>
2023-01-02 11:22:28 -0800

    no more hook scripts!

--- newMachineHookUser.sh
+++
@@ -1,5 +0,0 @@
-# import gpg keys
-gpg --import /etc/nixos/secrets/gpg/privkey.asc /etc/nixos/secrets/gpg/public.asc
-
-# grab passwords or create a new password repo
-git clone git@github.com:danielbarter/password_store.git /home/danielbarter/.password-store
