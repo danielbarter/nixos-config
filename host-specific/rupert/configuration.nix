a9ed55720617a5878789e1d826b6c1f71c58e5bb
Daniel Barter <danielbarter@gmail.com>
2023-01-15 10:20:23 -0800

    fixing bash syntax errors

--- host-specific/rupert/configuration.nix
+++ host-specific/rupert/configuration.nix
@@ -6,13 +6,13 @@ let
     executable = true;
     text = ''
          export LIBVIRT_DEFAULT_URI=qemu:///system
-         if [ $1 == "start" ]
+         if [ $1 -eq "start" ]
          then
            ${pkgs.libvirt}/bin/virsh start win10
            exit
          fi
 
-         if [ $1 == "status" ]
+         if [ $1 -eq "status" ]
          then
            ${pkgs.libvirt}/bin/virsh list --all
            exit
