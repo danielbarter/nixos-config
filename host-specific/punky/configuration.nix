28e84b6b5d51898ba6b65c2c43c589cb5cf54f86
Daniel Barter <danielbarter@gmail.com>
2023-01-27 15:57:54 -0800

    network config for punky

--- host-specific/punky/configuration.nix
+++ host-specific/punky/configuration.nix
@@ -1,8 +1,6 @@
 {config, pkgs, ...}:
 {
 
-  networking.hostName = "punky";
-
   services.nix-serve = {
     enable = true;
     port = 5000;
@@ -15,10 +13,54 @@
   '';
 
 
+  systemd.network = {
+    networks = {
+      "40-wlan0" = {
+        matchConfig = {
+          Name = "wlan0";
+        };
+
+        networkConfig = {
+          DHCP = "no";
+        };
+
+        addresses = [
+          { addressConfig = { Address = "192.168.1.11/24"; RouteMetric = 1024;}; }
+        ];
+
+        routes = [
+          { routeConfig = { Gateway = "192.168.1.1"; Metric = 1024;}; }
+        ];
+      };
+
+      "40-enp88s0" = {
+        matchConfig = {
+          Name = "enp37s0";
+        };
+
+        networkConfig = {
+          DHCP = "no";
+        };
+
+        addresses = [
+          { addressConfig = { Address = "192.168.1.10/24"; RouteMetric = 512;}; }
+        ];
+
+        routes = [
+          { routeConfig = { Gateway = "192.168.1.1"; Metric = 512;}; }
+        ];
+      };
+
+    };
+  };
+
 
 
   # these get put into /etc/hosts
   networking = {
+
+    hostName = "punky";
+
     hosts = {
       "192.168.1.1" = [ "asusmain" ];
       "192.168.1.2" = [ "asusaux" ];
