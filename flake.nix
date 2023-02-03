ea7a4c14942fb0ce6967c84c8fa0fe7cc5b35927
Daniel Barter <danielbarter@gmail.com>
2023-02-03 11:51:44 -0800

    emacs-overlay as flake input

--- flake.nix
+++ flake.nix
@@ -1,59 +1,66 @@
 {
-  inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-22.11;
-  inputs.hosts.url = github:StevenBlack/hosts;
+  inputs = {
+    nixpkgs.url = github:NixOS/nixpkgs/nixos-22.11;
+    hosts.url = github:StevenBlack/hosts;
+    emacs-overlay.url = github:nix-community/emacs-overlay;
+  };
 
-  outputs = { self, nixpkgs, hosts }: {
+  outputs = { self, nixpkgs, hosts, emacs-overlay }:
+    let pkgs-emacs-overlay = import nixpkgs {
+          system = "x86_64-linux";
+          overlays = [ emacs-overlay.overlays.default ];
+        };
+    in {
+      nixosConfigurations = {
+        jasper = nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit pkgs-emacs-overlay; };
+          system = "x86_64-linux";
+          modules =
+            [
+              ./base.nix
+              ./networking.nix
+              ./emacs.nix
+              ./users.nix
+              ./lingering.nix
+              ./pass.nix
+              ./home-setup.nix
+              ./jasper.nix
+              ./gui.nix
+            ];
+        };
 
-    nixosConfigurations = {
-      jasper = nixpkgs.lib.nixosSystem {
-        specialArgs = { inherit nixpkgs; system = "x86_64-linux"; };
-        system = "x86_64-linux";
-        modules =
-          [
-            ./base.nix
-            ./networking.nix
-            ./emacs.nix
-            ./users.nix
-            ./lingering.nix
-            ./pass.nix
-            ./home-setup.nix
-            ./jasper.nix
-            ./gui.nix
-          ];
-      };
+        punky = nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit pkgs-emacs-overlay; };
+          system = "x86_64-linux";
+          modules =
+            [
+              ./base.nix
+              ./networking.nix
+              ./emacs.nix
+              ./users.nix
+              ./lingering.nix
+              ./pass.nix
+              ./home-setup.nix
+              ./punky.nix
+              hosts.nixosModule { networking.stevenBlackHosts.enable = true; }
+            ];
+        };
 
-      punky = nixpkgs.lib.nixosSystem {
-        specialArgs = { inherit nixpkgs; system = "x86_64-linux"; };
-        system = "x86_64-linux";
-        modules =
-          [
-            ./base.nix
-            ./networking.nix
-            ./emacs.nix
-            ./users.nix
-            ./lingering.nix
-            ./pass.nix
-            ./home-setup.nix
-            ./punky.nix
-            hosts.nixosModule { networking.stevenBlackHosts.enable = true; }
-          ];
-      };
-
-      rupert = nixpkgs.lib.nixosSystem {
-        specialArgs = { inherit nixpkgs; system = "x86_64-linux"; };
-        system = "x86_64-linux";
-        modules =
-          [
-            ./base.nix
-            ./networking.nix
-            ./emacs.nix
-            ./users.nix
-            ./lingering.nix
-            ./pass.nix
-            ./home-setup.nix
-            ./rupert.nix
-          ];
+        rupert = nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit pkgs-emacs-overlay; };
+          system = "x86_64-linux";
+          modules =
+            [
+              ./base.nix
+              ./networking.nix
+              ./emacs.nix
+              ./users.nix
+              ./lingering.nix
+              ./pass.nix
+              ./home-setup.nix
+              ./rupert.nix
+            ];
+        };
       };
     };
-  };
 }
