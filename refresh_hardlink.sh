32b8880e3ccc448c36c400b23f2b4251471f2dab
Daniel Barter <danielbarter@gmail.com>
2023-02-03 08:44:34 -0800

    use refresh_hardlink as post-merge hook

--- refresh_hardlink.sh
+++ refresh_hardlink.sh
@@ -1,1 +1,3 @@
+# hard links become stale if upstream is updated
+# symlink to .git/hooks/post-merge
 ln -f /etc/nixos/host-specific/$(hostname)/configuration.nix /etc/nixos/host-specific-configuration.nix
