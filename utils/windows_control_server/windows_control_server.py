866c11348d5dba138bb766bedd6469b7c7d04d32
Daniel Barter <danielbarter@gmail.com>
2023-02-17 15:17:03 -0800

    added some global state to windows_control_server to prepare for using libvirt python bindings

--- utils/windows_control_server/windows_control_server.py
+++ utils/windows_control_server/windows_control_server.py
@@ -1,15 +1,25 @@
 import subprocess
 import os
 
-def run(environ, start_response):
-    status = '200 OK'
-    response_headers = [('Content-type', 'text/plain')]
-    start_response(status, response_headers)
-    if environ['QUERY_STRING'] == 'status':
-        result = subprocess.check_output(['windows', 'status'])
-        return [result]
-    elif environ['QUERY_STRING'] == 'start':
-        result = subprocess.check_output(['windows', 'start'])
-        return [result]
-    else:
-        return [b'usage: url?status or url?start']
+class WindowsControlServer:
+
+    def __init__(self):
+        self.count = 0
+
+    def __call__(self, environ, start_response):
+        self.count += 1
+        status = '200 OK'
+        response_headers = [('Content-type', 'text/plain')]
+        start_response(status, response_headers)
+        counter = f'counter: {self.count}\n\n\n'.encode()
+        if environ['QUERY_STRING'] == 'status':
+            result = subprocess.check_output(['windows', 'status'])
+            return [counter, result]
+        elif environ['QUERY_STRING'] == 'start':
+            result = subprocess.check_output(['windows', 'start'])
+            return [counter, result]
+        else:
+            return [b'usage: url?status or url?start']
+
+
+run = WindowsControlServer()
