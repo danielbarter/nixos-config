7efa74e677e51fca2c2d6a9d89a134769b5f6221
Daniel Barter <danielbarter@gmail.com>
2023-12-26 12:22:36 -0800

    have sway use gtk cursor

--- sway-gui.nix
+++ sway-gui.nix
@@ -38,6 +38,7 @@ let
         gsettings set $gnome_schema gtk-theme 'Dracula'
         gsettings set $gnome_schema icon-theme 'Adwaita'
         gsettings set $gnome_schema cursor-theme 'Dracula-cursors'
+        gsettings set $gnome_schema cursor-size 24
         gsettings set $gnome_schema document-font-name 'Source Sans Pro 11'
         gsettings set $gnome_schema font-name 'Source Sans Pro 11'
         gsettings set $gnome_schema monospace-font-name 'Source Code Pro 11'
