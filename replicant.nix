efa4a7fb49a0a5c7eac80268f490e14fc4b6cb1a
Daniel Barter <danielbarter@gmail.com>
2024-03-02 12:51:00 -0800

    tweaks to build_replicant_iso.sh

--- replicant.nix
+++ replicant.nix
@@ -1,18 +1,19 @@
-{pkgs, ...}: {
+{pkgs, flake, ...}: {
   system.stateVersion = "23.05";
 
   # set password to be empty for root
   users.users.root.initialPassword = "";
 
+
   # add encrypted, zipped nixos config to iso
   isoImage.contents = [
     {
-      source = /tmp/nixos.zip.gpg;
+      source = "/tmp/nixos.zip.gpg";
       target = "nixos.zip.gpg";
     }
 
     {
-      source = /etc/nixos/utils/setup_replicant_iso.sh;
+      source = "${flake.outPath}/utils/setup_replicant_iso.sh";
       target = "setup_replicant_iso.sh";
     }
   ];
--- utils/build_replicant_iso.sh
+++ utils/build_replicant_iso.sh
@@ -1,18 +1,12 @@
 build_new_replicant_iso() {
     echo "packing up nixos configuration"
-    cd /tmp
+    rm -f /tmp/nixos.zip.gpg
     sudo zip -r /tmp/nixos.zip /etc/nixos
     gpg -c /tmp/nixos.zip
     sudo rm /tmp/nixos.zip
 
     echo "generating iso"
-    cd /etc/nixos
-    nix build .#replicant-iso -o /tmp/result --impure
-    rm /tmp/nixos.zip.gpg
-    cp /tmp/result/iso/nixos.iso /tmp
-    rm /tmp/result
-
-    echo "all done. iso is at /tmp/nixos.iso"
+    nix build /etc/nixos#replicant-iso --impure
 }
 
 
