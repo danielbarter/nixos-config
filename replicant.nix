44ef71833551600c2fc4fc43754b7206e7bdfc8f
Daniel Barter <danielbarter@gmail.com>
2024-05-14 09:56:40 -0700

    added a console boot option to replicant

--- replicant.nix
+++ replicant.nix
@@ -38,12 +38,25 @@
           "/EFI/BOOT/BOOT${lib.toUpper efiArch}.EFI".source =
             "${pkgs.systemd}/lib/systemd/boot/efi/systemd-boot${efiArch}.efi";
 
+          "/loader/entries/loader.conf".source = pkgs.writeText "loader.conf" ''
+          default nixos.conf
+          timeout 10
+          '';
+
+          "/loader/entries/nixos_console.conf".source = pkgs.writeText "nixos_console.conf" ''
+          title NixOS (console)
+          linux /EFI/nixos/kernel.efi
+          initrd /EFI/nixos/initrd.efi
+          options init=${config.system.build.toplevel}/init ${toString config.boot.kernelParams} console=ttyS0
+          '';
+
           "/loader/entries/nixos.conf".source = pkgs.writeText "nixos.conf" ''
           title NixOS
           linux /EFI/nixos/kernel.efi
           initrd /EFI/nixos/initrd.efi
-          options init=${config.system.build.toplevel}/init ${toString config.boot.kernelParams} console=tty0 console=ttyS0
+          options init=${config.system.build.toplevel}/init ${toString config.boot.kernelParams}
           '';
+
 
           "/EFI/nixos/kernel.efi".source =
             "${config.boot.kernelPackages.kernel}/${config.system.boot.loader.kernelFile}";
