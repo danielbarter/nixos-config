c9d417b306e2a2e4c1b57b3c4481b6d22f04ce5a
Daniel Barter <danielbarter@gmail.com>
2024-05-19 18:27:17 -0700

    trying jhovold kernel

--- x13s.nix
+++ x13s.nix
@@ -16,6 +16,7 @@
       "i2c-hid"
       "i2c-hid-of"
       "i2c-qcom-geni"
+      "pcie-qcom"
       "phy-qcom-qmp-combo"
       "phy-qcom-qmp-pcie"
       "phy-qcom-qmp-usb"
@@ -23,7 +24,15 @@
       "phy-qcom-usb-hs"
     ];
 
-    kernelPackages = pkgs.linuxPackages_latest;
+    kernelPackages = pkgs.linuxPackagesFor (pkgs.buildLinux {
+      version = "6.9.0-rc7";
+      defconfig = "johan_defconfig";
+      src = pkgs.fetchurl {
+        url = "https://github.com/jhovold/linux/archive/refs/heads/wip/sc8280xp-6.9-rc7.tar.gz";
+        hash = "sha256-2cPRW6KXRVzJFIlIt+FgjmjSETuYSDFzRPPuxpvp/lM=";
+      };
+    });
+
     kernelParams = [
       "clk_ignore_unused"
       "pd_ignore_unused"
