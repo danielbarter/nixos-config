6d7605aa1b247d1094b5bd986cd5db4f8cd3cf0b
Daniel Barter <danielbarter@gmail.com>
2024-06-10 20:16:32 -0700

    using with in packages.nix for readability

--- packages.nix
+++ packages.nix
@@ -17,61 +17,61 @@
     let sway-enabled = config.programs.sway.enable;
         iwd-enabled = config.networking.wireless.iwd.enable;
         dev-machine = config.dev-machine;
-    in [
+    in with pkgs; [
 
-      pkgs.tmux # terminal multiplexer
-      pkgs.git
-      pkgs.file
-      pkgs.htop
-      pkgs.wget
-      pkgs.pciutils # lspci
-      pkgs.usbutils # lsusb
-      pkgs.nmap
+      tmux # terminal multiplexer
+      git
+      file
+      htop
+      wget
+      pciutils # lspci
+      usbutils # lsusb
+      nmap
 
       # we use dbus-python for the sway status bar
-      (if sway-enabled then (pkgs.python3.withPackages (p: [ p.dbus-python ])) else pkgs.python3)
+      (if sway-enabled then (python3.withPackages (p: [ p.dbus-python ])) else python3)
 
-      pkgs.zip
-      pkgs.unzip
-      pkgs.fzf # fuzzy searcher
-      (if sway-enabled then pkgs.emacs29-pgtk else pkgs.emacs29-nox)
-      pkgs.aspell
-      pkgs.aspellDicts.en
-      (pkgs.pass.withExtensions (exts: [ exts.pass-otp ]))
+      zip
+      unzip
+      fzf # fuzzy searcher
+      (if sway-enabled then emacs29-pgtk else emacs29-nox)
+      aspell
+      aspellDicts.en
+      (pass.withExtensions (exts: [ exts.pass-otp ]))
 
     ] ++ lib.optionals sway-enabled [
 
-      pkgs.alacritty # gpu accelerated terminal
-      pkgs.zathura # pdf viewer
-      pkgs.glib # gsettings
-      pkgs.dracula-theme # gtk theme
-      pkgs.gnome3.adwaita-icon-theme # default gnome icons
-      pkgs.swaylock
-      pkgs.swayidle
-      pkgs.grim # screenshot functionality
-      pkgs.slurp # screenshot functionality
-      pkgs.wl-clipboard
-      pkgs.mako # FreeDesktop notifications
-      pkgs.libnotify # notify-send
-      pkgs.kanshi # sway hotplug functionality
-      pkgs.bemenu
+      alacritty # gpu accelerated terminal
+      zathura # pdf viewer
+      glib # gsettings
+      dracula-theme # gtk theme
+      gnome3.adwaita-icon-theme # default gnome icons
+      swaylock
+      swayidle
+      grim # screenshot functionality
+      slurp # screenshot functionality
+      wl-clipboard
+      mako # FreeDesktop notifications
+      libnotify # notify-send
+      kanshi # sway hotplug functionality
+      bemenu
 
     ] ++ lib.optionals iwd-enabled [
 
-      pkgs.iw # linux tool for managing wireless networks
+      iw # linux tool for managing wireless networks
 
     ] ++ lib.optionals dev-machine [
 
-      pkgs.binutils # objdump, readelf and c++filt
-      pkgs.strace
-      pkgs.radare2
-      pkgs.direnv
-      pkgs.gdb
-      pkgs.cmake # cmake autocomplete and emacs mode.
-      pkgs.man-pages # linux programmers man pages
-      pkgs.man-pages-posix # posix man pages
-      pkgs.pyright
-      pkgs.nixd
+      binutils # objdump, readelf and c++filt
+      strace
+      radare2
+      direnv
+      gdb
+      cmake # cmake autocomplete and emacs mode.
+      man-pages # linux programmers man pages
+      man-pages-posix # posix man pages
+      pyright
+      nixd
 
     ];
 }
