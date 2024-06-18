0a55d2313e846d970229f60edba40fffa3fd3b28
Daniel Barter <danielbarter@gmail.com>
2024-06-18 11:00:34 -0700

    bumping flake

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
     # flake inputs can be overriden eg
     # --override-input nixpkgs /home/danielbarter/nixpkgs
-    nixpkgs.url = "github:NixOS/nixpkgs/release-24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
     hosts.url = "github:StevenBlack/hosts";
 
     # unify nixpkgs across inputs
