a6aef127890ddc52d7aeb8f4543a47ee8af1225d
Daniel Barter <danielbarter@gmail.com>
2024-07-11 14:25:19 -0700

    wirepluber disable seat monitoring

--- sound.nix
+++ sound.nix
@@ -10,7 +10,18 @@
     enable = true;
     alsa.enable = true;
     pulse.enable = true;
+
+    wireplumber = {
+      enable = true;
+      configPackages = [
+        (pkgs.writeTextDir "share/wireplumber/wireplumber.conf.d/10-bluez-seat.conf" ''
+          wireplumber.profiles = {
+            main = {
+              monitor.bluez.seat-monitoring = disabled
+            }
+          }
+       '')
+      ];
+    };
   };
-
-
 }
