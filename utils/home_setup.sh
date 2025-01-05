77d55df67aa07fae6d5a6f66ff8c02ff853a9810
Daniel Barter <danielbarter@gmail.com>
2025-01-05 07:47:03 -0800

    helix pyright

--- utils/home_setup.sh
+++ utils/home_setup.sh
@@ -1,6 +1,6 @@
 # symlink emacs config into home
 ln -sf /etc/nixos/dotfiles/emacs/emacs.el /home/danielbarter/.emacs.el
-ln -sf /etc/nixos/dotfiles/helix/config.toml /home/danielbarter/.config/helix/config.toml
+ln -sf /etc/nixos/dotfiles/helix /home/danielbarter/.config/helix
 
 # symlink shell config files into home
 ln -sf /etc/nixos/dotfiles/shell/alacritty.toml /home/danielbarter/.alacritty.toml
