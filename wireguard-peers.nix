213d95cee15f1138ee150d83b2357dd534430a98
Daniel Barter <danielbarter@gmail.com>
2025-03-06 10:28:31 -0800

    endpoints for wg

--- wireguard-peers.nix
+++ wireguard-peers.nix
@@ -2,10 +2,12 @@
   {
     PublicKey = (builtins.readFile ./public/wireguard/punky);
     AllowedIPs = [ "192.168.2.12" ];
+    EndPoint = [ "192.168.1.12:51820"]
   }
   
   {
     PublicKey = (builtins.readFile ./public/wireguard/jasper);
     AllowedIPs = [ "192.168.2.13" ];
+    EndPoint = [ "192.168.1.13:51820"]
   }
 ]
