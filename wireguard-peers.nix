d7d0df39dce22fa6f16bba0018741f0021eaf2ec
Daniel Barter <danielbarter@gmail.com>
2025-03-07 18:52:25 -0800

    systemd wants strings

--- wireguard-peers.nix
+++ wireguard-peers.nix
@@ -1,13 +1,13 @@
 let network-ids = import ./network-ids.nix;
 in [
   {
-    PublicKeyFile = /etc/nixos/public/wireguard/punky;
+    PublicKeyFile = "/etc/nixos/public/wireguard/punky";
     AllowedIPs = [ "192.168.2.${network-ids.punky}" ];
     Endpoint = [ "punkymeow.duckdns.org:51820"];
   }
   
   {
-    PublicKeyFile = /etc/nixos/public/wireguard/phone;
+    PublicKeyFile = "/etc/nixos/public/wireguard/phone";
     AllowedIPs = [ "192.168.2.${network-ids.phone}" ];
   }
 ]
