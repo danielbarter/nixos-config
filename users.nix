4d988b526067cc5984fef258b8d5576844daa458
Daniel Barter <danielbarter@gmail.com>
2025-03-07 18:34:16 -0800

    masking public keys

--- users.nix
+++ users.nix
@@ -3,8 +3,8 @@
   users.users = {
     annasavage = {
       isNormalUser = true;
-      openssh.authorizedKeys.keys = [
-        (builtins.readFile ./public/ssh/annasavage.pub)
+      openssh.authorizedKeys.keyFiles = [
+        /etc/nixos/public/ssh/annasavage.pub
       ];
     };
 
@@ -22,9 +22,9 @@
         "video"
         "audio"
       ];
-      openssh.authorizedKeys.keys = [
-        (builtins.readFile ./public/ssh/id_rsa.pub)
-        (builtins.readFile ./public/ssh/phone.pub)
+      openssh.authorizedKeys.keyFiles = [
+        /etc/nixos/public/ssh/id_rsa.pub
+        /etc/nixos/public/ssh/phone.pub
       ];
     };
   };
--- wireguard-peers.nix
+++ wireguard-peers.nix
@@ -1,13 +1,13 @@
 let network-ids = import ./network-ids.nix;
 in [
   {
-    PublicKey = (builtins.readFile ./public/wireguard/punky);
+    PublicKeyFile = /etc/nixos/public/wireguard/punky;
     AllowedIPs = [ "192.168.2.${network-ids.punky}" ];
     Endpoint = [ "punkymeow.duckdns.org:51820"];
   }
   
   {
-    PublicKey = (builtins.readFile ./public/wireguard/phone);
+    PublicKeyFile = /etc/nixos/public/wireguard/phone;
     AllowedIPs = [ "192.168.2.${network-ids.phone}" ];
   }
 ]
