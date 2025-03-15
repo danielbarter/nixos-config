9e5d9ac18cd11090096a0ca270a6dc884048d4e4
Daniel Barter <danielbarter@gmail.com>
2025-03-15 10:22:00 -0700

    blaze initial config

---
+++ blaze.nix
@@ -0,0 +1,47 @@
+# Edit this configuration file to define what should be installed on
+# your system. Help is available in the configuration.nix(5) man page, on
+# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
+
+{ config, lib, pkgs, ... }:
+
+{
+
+  boot.initrd.availableKernelModules = [ "xhci_pci" "nvme" "usbhid" "usb_storage" "sd_mod" "sdhci_pci" ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-intel" ];
+  boot.extraModulePackages = [ ];
+
+  fileSystems."/" =
+    { device = "/dev/disk/by-uuid/d0cd1c8b-869e-4dee-ae4f-c47901484d6f";
+      fsType = "ext4";
+    };
+
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-uuid/12CE-A600";
+      fsType = "vfat";
+      options = [ "fmask=0022" "dmask=0022" ];
+    };
+
+  swapDevices = [ ];
+
+
+  # catchall network config. Configure whatever interface is present
+  systemd.network.networks = {
+    "40-generic" = {
+      matchConfig = {
+        Name = "*";
+      };
+      networkConfig = {
+        DHCP = "yes";
+      };
+    };
+  };
+
+  networking.hostName = "blaze"
+
+  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+
+  system.stateVersion = "25.05"; # Did you read the comment?
+
+}
+
