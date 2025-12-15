242aafe04934f5f05a99cd4113e7875866fbb9ad
Daniel Barter <danielbarter@gmail.com>
2025-12-15 15:06:46 -0800

    bring back alacritty

--- utils/home_setup.sh
+++ utils/home_setup.sh
@@ -3,6 +3,7 @@ mkdir -p /home/danielbarter/.config
 ln -sf /etc/nixos/dotfiles/helix /home/danielbarter/.config
 
 # symlink shell config files into home
+ln -sf /etc/nixos/dotfiles/shell/alacritty.toml /home/danielbarter/.alacritty.toml
 ln -sf /etc/nixos/dotfiles/shell/bashrc /home/danielbarter/.bashrc
 ln -sf /etc/nixos/dotfiles/shell/bash_profile /home/danielbarter/.bash_profile
 ln -sf /etc/nixos/dotfiles/shell/tmux.conf /home/danielbarter/.tmux.conf
