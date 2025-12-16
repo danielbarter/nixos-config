9bed17c92750e803ffe3f023e5d46b89902d4dc4
Daniel Barter <danielbarter@gmail.com>
2025-12-15 20:07:36 -0800

    cleaning out wireless config

--- wireless.nix
+++ wireless.nix
@@ -1,18 +1,5 @@
 {pkgs,...}:
 {
-  
-  networking.wireless.iwd = {
-
-    enable = true;
-    settings = {
-      General = {
-        # attempt to find a better AP every 10 seconds (default is 60)
-        RoamRetryInterval = "10";
-      };
-    };
-  };
-
+  networking.wireless.iwd.enable = true;
   environment.systemPackages = [ pkgs.iw ];
-  # give wireless cards time to turn on
-  systemd.services.iwd.serviceConfig.ExecStartPre = "${pkgs.coreutils}/bin/sleep 2";
 }
