6055093f2f2fbdfcfc44e36f0ff9249bc233a74e
Daniel Barter <danielbarter@gmail.com>
2026-02-02 08:19:53 -0800

    sec_gen prompt

---
+++ prompts/sec_gen.md
@@ -0,0 +1,29 @@
+Write me a bash function, which generates fresh private/public keys using standard tools into a folder passed in as an argument with the following subfolders:
+
+secrets
+├── gpg
+│   └── privkey.asc
+├── nix
+│   └── private-key
+├── ssh
+│   └── id_rsa
+└── wireguard
+    ├── blaze
+    └── punky
+
+
+public
+├── gpg
+│   └── public.asc
+├── nix
+│   └── public-key
+├── ssh
+│   ├── id_rsa.pub
+└── wireguard
+    ├── blaze
+    └── punky
+
+
+the nix private-key/public-key pair is a nix signing key for substituters. It is fine if the function requires human interaction. Please use strong encryption protocols.
+
+Write a second function which sets the permissions correctly. Wireguard secrets should have owner and group systemd-network
--- utils/set_permissions.sh
+++ utils/set_permissions.sh
@@ -2,21 +2,12 @@
 chown -R danielbarter /etc/nixos
 chgrp -R users /etc/nixos
 
-# wireguard private keys only need to be read by systemd-network user
-chown -R systemd-network /etc/nixos/secrets/wireguard
-chgrp -R systemd-network /etc/nixos/secrets/wireguard
-
 # 660 for all files not in secrets
-for file in $(find /etc/nixos -path /etc/nixos/secrets -prune -o -type f -print)
+for file in $(find /etc/nixos -print)
 do
     chmod 660 $file
 done
 
-# 600 for all files in secrets
-for file in $(find /etc/nixos/secrets -type f)
-do
-    chmod 600 $file
-done
 
 # 770 for files in utils
 for file in $(find /etc/nixos/utils -type f -print)
@@ -24,8 +15,8 @@ do
     chmod 770 $file
 done
 
-
-for file in $(find /etc/nixos -type d)
+# 755 for directories
+for file in $(find /etc/nixos -type d -print)
 do
     chmod 775 $file
 done
