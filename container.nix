8b924e52ad83da71fa9ac0060280b527ccd15d7e
Daniel Barter <danielbarter@gmail.com>
2026-02-03 10:19:38 -0800

    standing up container

--- container.nix
+++ container.nix
@@ -1,19 +1,12 @@
 {
   pkgs,
-  config,
   modulesPath,
   ...
 }:
-
-let
-  pkgs2storeContents = map (x: {
-    object = x;
-    symlink = "none";
-  });
-in {
+{
 
   imports = [
-    "${modulesPath}/profiles/minimal.nix"
+    "${modulesPath}/profiles/docker-container.nix"
   ];
 
   boot.isNspawnContainer = true;
@@ -34,39 +27,6 @@ in {
     '';
 
 
-  networking.hostName = "nixos";
   system.stateVersion = "25.11";
   users.users.root.initialHashedPassword = "";
-
-
-  # Create the tarball
-  system.build.tarball = pkgs.callPackage (pkgs.path + "/nixos/lib/make-system-tarball.nix") {
-    contents = [
-      {
-        source = "${config.system.build.toplevel}/.";
-        target = "./";
-      }
-    ];
-    extraArgs = "--owner=0";
-
-    # Add init script to image
-    storeContents = pkgs2storeContents [
-      config.system.build.toplevel
-      pkgs.stdenv
-    ];
-  };
-
-  boot.postBootCommands = ''
-    # After booting, register the contents of the Nix store in the Nix
-    # database.
-    if [ -f /nix-path-registration ]; then
-      ${config.nix.package.out}/bin/nix-store --load-db < /nix-path-registration &&
-      rm /nix-path-registration
-    fi
-  '';
-
-  # Install init script
-  system.activationScripts.installInitScript = ''
-    ln -fs $systemConfig/init /init
-  '';
 }
