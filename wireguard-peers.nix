89e76c481532b73c69b048cdeff05e91e26aa5ac
Daniel Barter <danielbarter@gmail.com>
2026-08-13 18:23:56 -0700

    blaze not its own peer

--- wireguard-peers.nix
+++ wireguard-peers.nix
@@ -3,11 +3,5 @@ in [
   {
     PublicKeyFile = "/cold/public/wireguard/phone";
     AllowedIPs = [ "192.168.2.${network-ids.phone}" ];
-  }
-  
-  {
-    PublicKeyFile = "/cold/public/wireguard/blaze";
-    AllowedIPs = [ "192.168.2.${network-ids.blaze}" ];
-    Endpoint = [ "hobiehomelab.duckdns.org:51820"];
-  }
+  }  
 ]
